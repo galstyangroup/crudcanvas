40a599008a683467bd3c3554f1ed440021465869
Harut Galstyan <harut@galstyan.eu>
2019-04-11 10:39:45 +0200

    launch

--- examples/example.js
+++ examples/example.js
@@ -1,11 +1,73 @@
 /* eslint-disable no-console */
-const defaultAwesomeFunction = require('../lib').default;
-const { awesomeFunction } = require('../lib');
+const CrudCanvas = require('../lib').default;
 
-const defaultVal = defaultAwesomeFunction('Dinesh');
-const val = awesomeFunction();
 
-// defaultVal === 'I am the Default Awesome Function, fellow comrade! - Dinesh'
-console.log(defaultVal);
-// val === 'I am just an Awesome Function'
-console.log(val);
+const MilestoneState =  {
+    "id":"",
+    "rank":"",
+    "project":"",
+    "milestone":"",
+    "percentage":"",
+    "color":"",
+    "status":"",
+    "IsKey":"",
+    "expectedEndDate":"",
+    "actualEndDate":"",
+    "lastUpdated":"",
+    "reviewComments":""
+}
+
+if( !$("#CreateMilestoneViewBtn").length ){
+    $(".dt-buttons.btn-group").append(`
+        <a id="CreateMilestoneViewBtn" onclick="createView()" class="btn btn-default"> <i class="fas fa-list-ul"></i> Click here </a>
+    `);
+}
+
+
+//milestoneTemplate
+const milestoneTemplate = (milestone) => {
+let ModifiedDate =  moment.utc(milestone.ModifiedDate);
+debugger;
+return `
+<div class="card milestone">
+    <div class="card-body row">
+            <div class="col-sm-1 checkbox">
+                <i style="font-size:23px; color:${milestone.ActualEndDate !== null ? "green;" : "#a9a9a9;" } " class="text-center fas fa-check-square"></i>
+            </div>
+            <div class="col-sm-9">
+                <div class="col-sm-6">
+                    <b><p class=""> ${milestone.Description}  </p> </b>  
+                    <label class="label label-default" style="background-color:${milestone.StatusColor}"> ${milestone.StatusName}</label>
+                        <label title="${getText["project"]}" class="label label-default">${milestone.ProjectName}</label> 
+                    <br>
+                    
+                </div>
+                <div class="col-sm-6">
+                    <p>
+                        <label style="border-radius:50%;" class="label label-default status-${milestone.ColorName !== null ? milestone.ColorName: "0" }"  title="${milestone.ColorName}">${milestone.ColorID != null ? milestone.ColorID : 0}</label>        
+                        ${milestone.ReviewComments !== null ? milestone.ReviewComments : ""}
+                    </p>
+                    <p style="font-size:8px" title="${getText["updated"]}">
+                        ${ModifiedDate.fromNow()}
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-2 text-center">
+                <i class="fas fa-ellipsis-h rank-id-${milestone.Rank}"></i>
+                <div class="progress" style="height: 20px;" >
+                        <div class="progress-bar" role="progressbar" style="width: ${milestone.CompletedPercentage}%;" aria-valuenow=" ${milestone.CompletedPercentage}" aria-valuemin="0" aria-valuemax="100"> ${milestone.CompletedPercentage}%</div>
+                </div>
+            </div>
+    </div>
+</div>  
+`;
+}
+
+let SampleCrudCanvas = new CrudCanvas(
+"MilestoneCrud", 
+"/milestone/milestoneAPI", 
+"",
+milestoneTemplate, 
+MilestoneState, 
+"milestoneDatatable_wrapper" 
+);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
+class CrudCanvas {
+
+  constructor(name, dataUrl, authorisedActions, itemTemplate, crudModel, appendElement) {
+
+    this.name = name;
+    this.dataUrl = dataUrl;
+    this.authorisedActions = authorisedActions;
+    this.itemTemplate = itemTemplate;
+    this.crudModel = crudModel;
+    this._data = "";
+    this.appendElement = appendElement;
+  
+  }
+
+  getData() {
+      return fetch(this.dataUrl)
+          .then( res => res.json())
+          .then( data => this.createView(data) ) 
+  }
+
+
+  createView(data) {
+          let appendElement = this.appendElement;
+          document.getElementById(appendElement).innerHTML = ``;
+          
+          this.displayItems(data)
+  }
+
+  displayItems(data) {
+
+      //filterOptions for later implementation
+          //const OpenMilestones = CrudCanvas.HG_data.filter(x => x.ActualEndDate === null);
+          //const ClosedMilestones = CrudCanvas.HG_data.filter(x => x.ActualEndDate !== null);
+      
+      document.getElementById(this.appendElement).innerHTML += `
+      <br><br><h3>${getText["open-milestones"]}</h3><br>
+      ${data.map(this.itemTemplate).join("")}
+      `;
+  }
+
+  distroyItems() {
+      document.getElementById(this.appendElement).innerHTML = "";
+  }
+
+  createItem(data) {
+      CrudCanvas.HG_data.push(data);
+      this.distroyItems();
+      this.displayItems();
+  }
+  
+  deleteItem() {
+      
+  }
+
+}
+
+
 const defaultAwesomeFunction = (name) => {
   const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
   return returnStr;
@@ -5,6 +62,6 @@ const defaultAwesomeFunction = (name) => {
 
 const awesomeFunction = () => 'I am just an Awesome Function';
 
-export default defaultAwesomeFunction;
+export default CrudCanvas;
 
 export { awesomeFunction };
