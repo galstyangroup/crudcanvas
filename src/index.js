b3186126257462d5a45c13d41426af7d03c601cb
Harut Galstian <GALSTYAN@MacBook-Pro-van-Harut.local>
2019-04-11 11:00:55 +0200

    fix js errors

--- src/index.js
+++ src/index.js
@@ -1,67 +1,57 @@
+const fetch = require('node-fetch');
+
 class CrudCanvas {
-
   constructor(name, dataUrl, authorisedActions, itemTemplate, crudModel, appendElement) {
-
     this.name = name;
     this.dataUrl = dataUrl;
     this.authorisedActions = authorisedActions;
     this.itemTemplate = itemTemplate;
     this.crudModel = crudModel;
-    this.g_data = "";
+    this.g_data = '';
     this.appendElement = appendElement;
-  
   }
 
   getData() {
-      return fetch(this.dataUrl)
-          .then( res => res.json())
-          .then( data => this.createView(data) ) 
+    return fetch(this.dataUrl)
+      .then(res => res.json())
+      .then(data => this.createView(data));
   }
 
 
   createView(data) {
-          let appendElement = this.appendElement;
-          document.getElementById(appendElement).innerHTML = ``;
-          
-          this.displayItems(data)
+    // eslint-disable-next-line prefer-destructuring
+    const appendElement = this.appendElement;
+    // eslint-disable-next-line no-undef
+    document.getElementById(appendElement).innerHTML = '';
+    this.displayItems(data);
   }
 
   displayItems(data) {
-
-      //filterOptions for later implementation
-          //const OpenMilestones = CrudCanvas.HG_data.filter(x => x.ActualEndDate === null);
-          //const ClosedMilestones = CrudCanvas.HG_data.filter(x => x.ActualEndDate !== null);
-      
-      document.getElementById(this.appendElement).innerHTML += `
+    // filterOptions for later implementation
+    // const OpenMilestones = CrudCanvas.HG_data.filter(x => x.ActualEndDate === null);
+    // const ClosedMilestones = CrudCanvas.HG_data.filter(x => x.ActualEndDate !== null);
+    // eslint-disable-next-line no-undef
+    document.getElementById(this.appendElement).innerHTML += `
       <br><br><h3>Items</h3><br>
-      ${data.map(this.itemTemplate).join("")}
+      ${data.map(this.itemTemplate).join('')}
       `;
   }
 
   distroyItems() {
-      document.getElementById(this.appendElement).innerHTML = "";
+    // eslint-disable-next-line no-undef
+    document.getElementById(this.appendElement).innerHTML = '';
   }
 
   createItem(data) {
-      CrudCanvas.HG_data.push(data);
-      this.distroyItems();
-      this.displayItems();
-  }
-  
-  deleteItem() {
-      
+    CrudCanvas.HG_data.push(data);
+    this.distroyItems();
+    this.displayItems();
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  deleteItem() {
+
+  }
 }
 
-
-const defaultAwesomeFunction = (name) => {
-  const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
-  return returnStr;
-};
-
-const awesomeFunction = () => 'I am just an Awesome Function';
-
 export default CrudCanvas;
-
-export { awesomeFunction };
